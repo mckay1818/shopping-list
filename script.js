cdea6b6f9d33c83c787eca1b8618aa61173f6eeb
mckay1818 <72710253+mckay1818@users.noreply.github.com>
2021-01-29 20:55:12 -0500

    'done' class is toggled on user-added items; delete buttons made

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 var listItems = document.getElementsByTagName("li");
+var deleteBtns = document.getElementsByClassName("delete");
 
 function inputLength() {
 	return input.value.length;
@@ -12,6 +13,11 @@ function createListElement() {
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	addLiEvent();
+	// addBtnEvent();
+	var deleteBtn = document.createElement("button");
+	deleteBtn.classList.add("delete");
+	deleteBtn.innerHTML = "Delete";
+	li.insertAdjacentElement("afterend", deleteBtn);
 	input.value = "";
 }
 
@@ -26,9 +32,21 @@ function addListAfterKeypress(event) {
 		createListElement();
 	}
 }
+	
+
+function removeParent() {
+	this.parentNode.remove();
+}
 
 function toggleDone(event) {
 	event.currentTarget.classList.toggle("done");
+}
+
+function addBtnEvent() {
+	//can't do forEach bc HTMLCollection is NOT an array
+	for (let i = 0; i < deleteBtns.length; i++) {
+		deleteBtns[i].addEventListener("click", removeParent);
+	}
 }
 
 function addLiEvent() {
@@ -37,6 +55,8 @@ function addLiEvent() {
 		listItems[i].addEventListener("click", toggleDone);
 	}
 }
+
 addLiEvent();
+addBtnEvent();
 button.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
